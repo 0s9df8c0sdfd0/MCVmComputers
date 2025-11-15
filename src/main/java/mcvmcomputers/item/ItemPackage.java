428f693089315bc0bf555abebdcb41f7379206d5
0s9df8c0sdfd0 <159628909+0s9df8c0sdfd0@users.noreply.github.com>
2025-11-15 11:00:11 -0500

    Refactor ItemPackage class for new API usage

--- src/main/java/mcvmcomputers/item/ItemPackage.java
+++ src/main/java/mcvmcomputers/item/ItemPackage.java
@@ -2,61 +2,61 @@ package mcvmcomputers.item;
 
 import java.util.List;
 
-import net.minecraft.client.item.TooltipContext;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Formatting;
-import net.minecraft.util.Hand;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.World;
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 
-public class ItemPackage extends Item{
-
-	public ItemPackage(Settings settings) {
-		super(settings);
-	}
-	
-	@Override
-	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-		if(!world.isClient){
-			ItemStack is = user.getStackInHand(hand);
-			if(is.getTag() != null) {
-				if(is.getTag().contains("packaged_item")) {
-					is.decrement(1);
-					user.giveItemStack(new ItemStack(Registry.ITEM.get(new Identifier(is.getTag().getString("packaged_item")))));
-				}
-			}
-		}
-		return super.use(world, user, hand);
-	}
-	
-	@Override
-	public Text getName(ItemStack stack) {
-		if(stack.getTag() != null) {
-			if(stack.getTag().contains("packaged_item")) {
-				return new TranslatableText("mcvmcomputers.packaged").formatted(Formatting.GRAY).append(new TranslatableText(Registry.ITEM.get(new Identifier(stack.getTag().getString("packaged_item"))).getTranslationKey()).formatted(Formatting.GREEN));
-			}
-		}
-		return new TranslatableText("mcvmcomputers.invalid_package").formatted(Formatting.RED);
-	}
-	
-	@Override
-	public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
-		tooltip.add(new TranslatableText("mcvmcomputers.open_with_right_click").formatted(Formatting.GRAY));
-	}
-	
-	public static ItemStack createPackage(Identifier id) {
-		ItemStack is = new ItemStack(ItemList.ITEM_PACKAGE);
-		CompoundTag ct = is.getOrCreateTag();
-		ct.putString("packaged_item", id.toString());
-		is.setTag(ct);
-		return is;
-	}
-
+public class ItemPackage extends Item {
+    public ItemPackage(Properties properties) {
+        super(properties);
+    }
+    
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        if(!level.isClientSide) {
+            ItemStack is = player.getItemInHand(hand);
+            if(is.getTag() != null) {
+                if(is.getTag().contains("packaged_item")) {
+                    is.shrink(1);
+                    Item item = BuiltInRegistries.ITEM.get(new ResourceLocation(is.getTag().getString("packaged_item")));
+                    player.addItem(new ItemStack(item));
+                }
+            }
+        }
+        return super.use(level, player, hand);
+    }
+    
+    @Override
+    public Component getName(ItemStack stack) {
+        if(stack.getTag() != null) {
+            if(stack.getTag().contains("packaged_item")) {
+                Item item = BuiltInRegistries.ITEM.get(new ResourceLocation(stack.getTag().getString("packaged_item")));
+                return Component.translatable("mcvmcomputers.packaged")
+                    .withStyle(ChatFormatting.GRAY)
+                    .append(Component.translatable(item.getDescriptionId())
+                        .withStyle(ChatFormatting.GREEN));
+            }
+        }
+        return Component.translatable("mcvmcomputers.invalid_package").withStyle(ChatFormatting.RED);
+    }
+    
+    @Override
+    public void appendHoverText(ItemStack stack, Level level, List<Component> tooltip, TooltipFlag flag) {
+        tooltip.add(Component.translatable("mcvmcomputers.open_with_right_click")
+            .withStyle(ChatFormatting.GRAY));
+    }
+    
+    public static ItemStack createPackage(ResourceLocation id) {
+        ItemStack is = new ItemStack(ItemList.ITEM_PACKAGE.get());
+        is.getOrCreateTag().putString("packaged_item", id.toString());
+        return is;
+    }
 }
