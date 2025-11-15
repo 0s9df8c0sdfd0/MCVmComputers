be05e324a7053fdda31faa0505a7fa707f5cf528
0s9df8c0sdfd0 <159628909+0s9df8c0sdfd0@users.noreply.github.com>
2025-11-15 10:58:03 -0500

    Refactor ItemHarddrive class for updated API usage

--- src/main/java/mcvmcomputers/item/ItemHarddrive.java
+++ src/main/java/mcvmcomputers/item/ItemHarddrive.java
@@ -1,50 +1,43 @@
 package mcvmcomputers.item;
 
 import mcvmcomputers.MainMod;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Formatting;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.world.World;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
-public class ItemHarddrive extends OrderableItem{
-	public ItemHarddrive(Settings settings) {
-		super(settings, 6);
-	}
-	
-	@Override
-	public boolean shouldSyncTagToClient() {
-		return true;
-	}
-	
-	@Override
-	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-		if(world.isClient) {
-			MainMod.hardDriveClick.run();
-		}
-		return super.use(world, user, hand);
-	}
-	
-	@Override
-	public Text getName(ItemStack stack) {
-		if(stack.getTag() != null) {
-			if(stack.getTag().contains("vhdfile")) {
-				return new TranslatableText("mcvmcomputers.hdd_item_name", stack.getTag().getString("vhdfile")).formatted(Formatting.WHITE);
-			}
-		}
-		return new TranslatableText("mcvmcomputers.hdd_item_name", new TranslatableText("mcvmcomputers.hdd_right_click").asString()).formatted(Formatting.WHITE);
-	}
-	
-	public static ItemStack createHardDrive(String fileName) {
-		ItemStack is = new ItemStack(ItemList.ITEM_HARDDRIVE);
-		CompoundTag ct = is.getOrCreateTag();
-		ct.putString("vhdfile", fileName);
-		is.setTag(ct);
-		return is;
-	}
-
+public class ItemHarddrive extends OrderableItem {
+    public ItemHarddrive(Properties properties) {
+        super(properties, 6);
+    }
+    
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        if(level.isClientSide) {
+            MainMod.hardDriveClick.run();
+        }
+        return super.use(level, player, hand);
+    }
+    
+    @Override
+    public Component getName(ItemStack stack) {
+        if(stack.getTag() != null) {
+            if(stack.getTag().contains("vhdfile")) {
+                return Component.translatable("mcvmcomputers.hdd_item_name", stack.getTag().getString("vhdfile"));
+            }
+        }
+        return Component.translatable("mcvmcomputers.hdd_item_name", 
+            Component.translatable("mcvmcomputers.hdd_right_click").getString());
+    }
+    
+    public static ItemStack createHardDrive(String fileName) {
+        ItemStack is = new ItemStack(ItemList.ITEM_HARDDRIVE.get());
+        CompoundTag ct = is.getOrCreateTag();
+        ct.putString("vhdfile", fileName);
+        is.setTag(ct);
+        return is;
+    }
 }
